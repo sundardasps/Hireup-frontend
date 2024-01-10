8ce612fb7bac738c875c95456b2a55c3001fafbb
Sundardasps <sundardasps0055@gmail.com>
2024-01-10 18:54:43 +0530

    deployment 4

--- src/Pages/userPages/userHomePage/Profile.jsx
+++ src/Pages/userPages/userHomePage/Profile.jsx
@@ -1,6 +1,6 @@
 
 import UserDetails from '../../../Components/userComponents/userProfiieComponents/UserDetails'
-import Sidebar from '../../../Components/userComponents/userProfiieComponents/ProfileSidebar'
+
 
 
 function Profile() {
