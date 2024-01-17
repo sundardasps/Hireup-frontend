75d4988deac53f1943c2e359e18e165418b99005
Sundardasps <sundardasps0055@gmail.com>
2024-01-17 14:37:49 +0530

    user job apply ui updations

--- src/Components/userComponents/userDialogs/ViewResume.jsx
+++ src/Components/userComponents/userDialogs/ViewResume.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Dialog,
-  Card,
-} from "@material-tailwind/react";
+import { Button, Dialog, Card } from "@material-tailwind/react";
 
 export function ViewResume({ data }) {
   const [open, setOpen] = React.useState(false);
@@ -19,8 +15,8 @@ export function ViewResume({ data }) {
         handler={handleOpen}
         className="bg-transparent shadow-none"
       >
-        <Card className="mx-auto w-full h-full">
-          <iframe src={data} width="" height="500rem" title="PDF-file"></iframe>
+        <Card className="mx-auto w-full sm:w-96 h-full">
+          <iframe src={data} width="100%" height="500rem" title="PDF-file"></iframe>
         </Card>
       </Dialog>
     </>
